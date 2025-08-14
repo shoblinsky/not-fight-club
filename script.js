429e405adc9e0686473fcf1a28b97fef21ea16e3
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-08-14 16:38:15 +0500

    feat: add registration and enableButton function

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Huh')
+const registrationButton = document.getElementById('registration')
+const nicknameInput = document.getElementById('nickname');
+
+const enableButton = () => {
+    const nickname = nicknameInput.value.trim();
+    registrationButton.disabled = nickname === '';
+};
+
+nicknameInput.addEventListener('input', enableButton);
+
+
+const registrate = () => {
+    const nickname = nicknameInput.value;
+    if (nickname.trim() !== '') {
+        window.localStorage.setItem('name', nickname);
+        console.log(`Welcome, ${nickname}!`);
+    } else {
+        window.localStorage.setItem('name', 'Unnamed Warrior');
+        console.log(`Welcome, Unnamed Warrior! You always can change your nickname during the game.`);
+    }
+}
+
+
+registrationButton.addEventListener('click', registrate);
+
+
+
