3e4b35c687f9e0a0ffd96731bfefb3055d751219
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-08-28 18:58:16 +0500

    feat: add some querrySelector, add autoscroll to log

--- battle.js
+++ battle.js
@@ -1,7 +1,23 @@
 const logWindow = document.getElementById('log__window');
 const fightWrapper = document.getElementById('fight__wrapper');
 
-import enemies from "./hostiles.js";
+const playerHealthSpan = document.querySelector('.player__health');
+const enemyHealthSpan = document.querySelector('.enemy__health');
+
+const playerHpBar = document.querySelector('.hpbar__player');
+const enemyHpBar = document.querySelector('.hpbar__enemy');
+
+const playerNameSpan = document.querySelector('player__name__fight')
+const playerAvatar = document.querySelector('player__avatar__fight')
+
+const enemyNameSpan = document.querySelector('.enemy__name__fight');
+const enemyAvatar = document.querySelector('.enemy__avatar__fight');
+
+
+const attack = document.querySelector('.attack');
+
+
+import { enemies, activeEnemy, activeEnemyIndex, chooseEnemyIndex } from "./hostiles.js";
 const personParts = ['head', 'torso', 'hands', 'belly', 'legs']
 
 let playerIsDead = false;
@@ -10,7 +26,8 @@ let isGameOver = false;
 
 function gameOver() {
     if (playerIsDead || enemyIsDead) {
-        isGameOver = true
+        isGameOver = true;
+        attackButton.disabled = true;
     }
 }
 
@@ -33,7 +50,8 @@ function resetLog() {
 function addLog(string) {
     const logString = document.createElement('p')
     logString.textContent = `Turn #${turn}. ${string}`
-    logWindow.appendChild(logString)
+    logWindow.appendChild(logString);
+    logWindow.scrollTop = logWindow.scrollHeight;
     turn = turn + 1;
 }
 
